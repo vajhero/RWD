44d5d2ba2a9b9e29224d949b34b0c0b238c637bc
Artur Karczewski <83916784+vajhero@users.noreply.github.com>
2021-12-22 09:14:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let WinOpen_karczewski2 = () => {
+    window.open("zadanie2.html");
+}
+
+let WinOpen_karczewski3 = () => {
+    window.open("zadanie3.html");
+}
+
+let WinOpen_karczewski4 = () => {
+    window.open("zadanie4.html");
+}
+
+let WinOpen_karczewski2_kod = () => {
+    window.open("zadanie2_kod.html");
+}
+
+let WinOpen_karczewski3_kod = () => {
+    window.open("zadanie3_kod.html");
+}
+
+let WinOpen_karczewski4_kod = () => {
+    window.open("zadanie4_kod.html");
+}
